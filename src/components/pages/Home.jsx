40871c99b4a0c870d4dad32cf0f4c7d6d5898ead
Sijan666 <666majharulislam@gmail.com>
2026-02-03 16:04:27 +0600

    add : category in home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../layouts/Banner'
+import Category from '../layouts/Category'
 
 const Home = () => {
     return (
         <>
         <Banner/>
+        <Category/>
         </>
     )
 }
