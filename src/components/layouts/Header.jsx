17c3129dde214429b2e24bccc421a9a469aad712
jaber-2004 <md9292463@gmail.com>
2026-02-05 00:26:34 +0600

    link done

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -7,6 +7,7 @@ import { FaRegUser } from "react-icons/fa";
 import { AiOutlineHeart } from "react-icons/ai";
 import { FaShoppingBasket } from "react-icons/fa";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   let [show, setShow] = useState(false);
@@ -103,18 +104,42 @@ const Header = () => {
                 Saree
               </h4>
               <ul className="flex flex-col gap-y-3.75 text-[#757575] text-sm font-Poppins">
-                <li>Cotton</li>
-                <li>Silk</li>
-                <li>Katan</li>
-                <li>Muslin</li>
-                <li>Jamdani</li>
-                <li>Georgette</li>
-                <li>Nakshi Kantha</li>
-                <li>Linen</li>
-                <li>Khadi</li>
-                <li>Tangail Taant</li>
-                <li>Monipuri</li>
-                <li>Dhakai Benaroshi</li>
+                <li>
+                  <Link>Cotton</Link>
+                </li>
+                <li>
+                  <Link>Silk</Link>
+                </li>
+                <li>
+                  <Link>Katan</Link>
+                </li>
+                <li>
+                  <Link>Muslin</Link>
+                </li>
+                <li>
+                  <Link>Jamdani</Link>
+                </li>
+                <li>
+                  <Link>Georgette</Link>
+                </li>
+                <li>
+                  <Link>Nakshi Kantha</Link>
+                </li>
+                <li>
+                  <Link>Linen</Link>
+                </li>
+                <li>
+                  <Link>Khadi</Link>
+                </li>
+                <li>
+                  <Link>Tangail Taant</Link>
+                </li>
+                <li>
+                  <Link>Monipuri</Link>
+                </li>
+                <li>
+                  <Link>Dhakai Benaroshi</Link>
+                </li>
               </ul>
             </div>
             {/* Flex Two start  */}
@@ -123,18 +148,42 @@ const Header = () => {
                 Clothes
               </h4>
               <ul className="flex flex-col gap-y-3.75 text-[#757575] text-sm font-Poppins">
-                <li>T-shirt</li>
-                <li>Shorts</li>
-                <li>Shirt</li>
-                <li>Tracksuit</li>
-                <li>Jeans</li>
-                <li>Georgette</li>
-                <li>Waistcoat</li>
-                <li>Sweater</li>
-                <li>coats</li>
-                <li>Suit</li>
-                <li>Sweatshirt</li>
-                <li>Uniform</li>
+                <li>
+                  <Link>T-shirt</Link>
+                </li>
+                <li>
+                  <Link>Shorts</Link>
+                </li>
+                <li>
+                  <Link>Shirt</Link>
+                </li>
+                <li>
+                  <Link>Tracksuit</Link>
+                </li>
+                <li>
+                  <Link>Jeans</Link>
+                </li>
+                <li>
+                  <Link>Georgette</Link>
+                </li>
+                <li>
+                  <Link>Waistcoat</Link>
+                </li>
+                <li>
+                  <Link>Sweater</Link>
+                </li>
+                <li>
+                  <Link>coats</Link>
+                </li>
+                <li>
+                  <Link>Suit</Link>
+                </li>
+                <li>
+                  <Link>Sweatshirt</Link>
+                </li>
+                <li>
+                  <Link>Uniform</Link>
+                </li>
               </ul>
             </div>
             {/* Flex Three start  */}
@@ -143,18 +192,42 @@ const Header = () => {
                 Cosmetic
               </h4>
               <ul className="flex flex-col gap-y-3.75 text-[#757575] text-sm font-Poppins">
-                <li>Perfume</li>
-                <li>Eye make-up</li>
-                <li>Skin care</li>
-                <li>Hair care</li>
-                <li>Make-up</li>
-                <li>Oral Care</li>
-                <li>Body Care</li>
-                <li>Sanitary pad</li>
-                <li>Shower Gels & Creams</li>
-                <li>Hair Removal Products</li>
-                <li>Lipstick</li>
-                <li>Suntan cream</li>
+                <li>
+                  <Link>Perfume</Link>
+                </li>
+                <li>
+                  <Link>Eye make-up</Link>
+                </li>
+                <li>
+                  <Link>Skin care</Link>
+                </li>
+                <li>
+                  <Link>Hair care</Link>
+                </li>
+                <li>
+                  <Link>Make-up</Link>
+                </li>
+                <li>
+                  <Link>Oral Care</Link>
+                </li>
+                <li>
+                  <Link>Body Care</Link>
+                </li>
+                <li>
+                  <Link>Sanitary pad</Link>
+                </li>
+                <li>
+                  <Link>Shower Gels & Creams</Link>
+                </li>
+                <li>
+                  <Link>Hair Removal Products</Link>
+                </li>
+                <li>
+                  <Link>Lipstick</Link>
+                </li>
+                <li>
+                  <Link>Suntan cream</Link>
+                </li>
               </ul>
             </div>
             {/* Flex Four start */}
@@ -163,18 +236,42 @@ const Header = () => {
                 Sports & Outdoor
               </h4>
               <ul className="flex flex-col gap-y-3.75 text-[#757575] text-sm font-Poppins">
-                <li>Sweatshirt</li>
-                <li>T-shirt</li>
-                <li>Sports Bra</li>
-                <li>Leggings</li>
-                <li>Tracksuit</li>
-                <li>Running Shoes</li>
-                <li>Sports Bag</li>
-                <li>Sports Equipment</li>
-                <li>Outdoor Shoes</li>
-                <li>Snow Boot</li>
-                <li>Outdoor Equipment</li>
-                <li>Sports Accessories</li>
+                <li>
+                  <Link>Sweatshirt</Link>
+                </li>
+                <li>
+                  <Link>T-shirt</Link>
+                </li>
+                <li>
+                  <Link>Sports Bra</Link>
+                </li>
+                <li>
+                  <Link>Leggings</Link>
+                </li>
+                <li>
+                  <Link>Tracksuit</Link>
+                </li>
+                <li>
+                  <Link>Running Shoes</Link>
+                </li>
+                <li>
+                  <Link>Sports Bag</Link>
+                </li>
+                <li>
+                  <Link>Sports Equipment</Link>
+                </li>
+                <li>
+                  <Link>Outdoor Shoes</Link>
+                </li>
+                <li>
+                  <Link>Snow Boot</Link>
+                </li>
+                <li>
+                  <Link>Outdoor Equipment</Link>
+                </li>
+                <li>
+                  <Link>Sports Accessories</Link>
+                </li>
               </ul>
             </div>
             {/* Flex Five start */}
@@ -183,22 +280,42 @@ const Header = () => {
                 Personal Care
               </h4>
               <ul className="flex flex-col gap-y-3.75 text-[#757575] text-sm font-Poppins">
-                <li>Perfume</li>
-                <li>Sexual Health</li>
-                <li>After Shave Products</li>
-                <li>Razor blade</li>
+                <li>
+                  <Link>Perfume</Link>
+                </li>
+                <li>
+                  <Link>Sexual Health</Link>
+                </li>
+                <li>
+                  <Link>After Shave Products</Link>
+                </li>
+                <li>
+                  <Link>Razor blade</Link>
+                </li>
               </ul>
               <div className="mt-10.75">
                 <h4 className="text-[#3D3D3F] text-base font-medium font-Poppins pb-3">
                   Shoes
                 </h4>
                 <ul className="flex flex-col gap-y-3.75 text-[#757575] text-sm font-Poppins">
-                  <li>Heeled shoes</li>
-                  <li>sneakers</li>
-                  <li>Casual Shoes</li>
-                  <li>Babet</li>
-                  <li>Sandals</li>
-                  <li>Slipper</li>
+                  <li>
+                    <Link>Heeled shoes</Link>
+                  </li>
+                  <li>
+                    <Link>sneakers</Link>
+                  </li>
+                  <li>
+                    <Link>Casual Shoes</Link>
+                  </li>
+                  <li>
+                    <Link>Babet</Link>
+                  </li>
+                  <li>
+                    <Link>Sandals</Link>
+                  </li>
+                  <li>
+                    <Link>Slipper</Link>
+                  </li>
                 </ul>
               </div>
             </div>
