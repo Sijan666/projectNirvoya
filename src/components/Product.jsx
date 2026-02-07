041ea4e4a2697975f38b605975d2cf188c0cb26d
Sijan666 <666majharulislam@gmail.com>
2026-02-08 03:59:45 +0600

    fix :  some changes in product for responsive xs

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Badge from "./Badge";
 import Images from "./Images";
 import Flex from "./Flex"; 
@@ -18,28 +18,35 @@ const Product = ({
     };
   
   return (
-    <div> 
-      <div className="bg-white px-[9px] pt-[9px] pb-[15px] border-2 border-[#E9E9E9] rounded-[10px] inline-block relative">
-        <Images className={'cursor-pointer'} imgSrc={productImg}/>
+    <div className="w-full px-1"> 
+      <div className="bg-white p-2.5 border border-[#E9E9E9] rounded-[10px] relative w-full overflow-hidden group">
+        {/* image */}
+        <div className="w-full overflow-hidden rounded-[5px]">
+            <Images className={'cursor-pointer w-full h-auto object-cover'} imgSrc={productImg}/>
+        </div>
+        {/* badge */}
         {badgeText && <Badge className={"absolute top-[9px] left-[9px]"} badgeText={badgeText}/>}
-        <FaHeart 
-          onClick={handleLike}
-          className={`absolute top-[25px] right-[25px] text-[25px] cursor-pointer transition-all duration-300 ${isLiked ? 'text-red-500' : 'text-gray-300'}`} />
+        {/* heart icon */}
+        <FaHeart onClick={handleLike} className={`absolute top-[25px] right-[25px] text-[20px] lg:text-[25px] cursor-pointer transition-all duration-300 ${isLiked ? 'text-red-500' : 'text-gray-300'}`} />
+        {/* lower part */}
         <div className="mt-4">
-          <div className="flex gap-x-2">
-            <div className={`flex space-x-0.5 text-base text-[#FAC96B] cursor-pointer ${className}`}>
+          <div className="flex gap-x-2 items-center">
+            {/* rating */}
+            <div className={`flex space-x-0.5 text-sm lg:text-base text-[#FAC96B] cursor-pointer ${className}`}>
               {productRating}
             </div>
-            <p className="text-[12px]">(0)</p>
+            <p className="text-[10px] lg:text-[12px] text-gray-500">(0)</p>
           </div>
-          <h3 className="w-[294px] h-12 py-2.5 font-normal text-[18px] text-[#383838] leading-[150%]">
+          {/* title */}
+          <h3 className="w-full py-2.5 text-sm lg:text-[18px] text-[#383838] leading-[150%] h-15">
             {productTitle}
           </h3>        
-          <Flex className="justify-between items-center pt-5">
-            <p className="font-medium text-[22px] text-[#0970CD]">
+          <Flex className="justify-between items-center pt-2 lg:pt-5">
+            {/* price */}
+            <p className="font-medium text-base lg:text-[22px] text-[#0970CD]">
               {productPrice}
             </p>
-            <FaBasketShopping className="text-[24px] text-[#0198E9] cursor-pointer" />
+            <FaBasketShopping className="text-[20px] lg:text-[24px] text-[#0198E9] cursor-pointer hover:text-[#0970CD] transition-colors" />
           </Flex>
         </div>
       </div>
