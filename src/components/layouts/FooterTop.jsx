c97804677b8347b6ccca76118c1c37a1ef31ea84
Sijan666 <666majharulislam@gmail.com>
2026-06-04 03:07:16 +0600

    fix responsive issue in footerTop

--- src/components/layouts/FooterTop.jsx
+++ src/components/layouts/FooterTop.jsx
@@ -9,13 +9,13 @@ import Container from "../Container";
 const FooterTop = () => {
   return (
     <>
-      <div className=" lg:mt-10 w-[420px] lg:w-full">
-        <Container className={'lg:w-[1405px] w-[395px]'}>
-          <div className="flex lg:items-center lg:justify-between py-10 flex-col lg:flex-row gap-y-5 items-start">
-            <Images imgSrc={Footertop1} className={''}/>
-            <Images imgSrc={Footertop2} className={''}/>
-            <Images imgSrc={Footertop3} className={''}/>
-            <Images imgSrc={Footertop4} className={''}/>
+      <div className="w-full lg:mt-10 overflow-hidden">
+        <Container className={'w-full max-w-[1405px] mx-auto px-4 lg:px-0'}>
+          <div className="flex flex-col sm:flex-row flex-wrap items-center justify-center lg:justify-between py-8 lg:py-10 gap-y-8 sm:gap-x-10 lg:gap-x-0 lg:gap-y-0">
+            <Images imgSrc={Footertop1} className="w-auto max-w-[200px] lg:max-w-none" />
+            <Images imgSrc={Footertop2} className="w-auto max-w-[200px] lg:max-w-none" />
+            <Images imgSrc={Footertop3} className="w-auto max-w-[200px] lg:max-w-none" />
+            <Images imgSrc={Footertop4} className="w-auto max-w-[200px] lg:max-w-none" />
           </div>
         </Container>
       </div>
