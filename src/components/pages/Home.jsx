3d42922ff01d48abf28c34450f955275e978e8c1
Sijan666 <666majharulislam@gmail.com>
2026-06-05 22:56:33 +0600

    add react helmet in home for seo

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import Banner from '../layouts/Banner'
 import Category from '../layouts/Category'
 import FlashDeals from '../layouts/FlashDeals'
 import FeaturedProduct from '../layouts/FeaturedProduct'
 
-
 const Home = () => {
         return (
         <>
+        <Helmet>
+                <title>Home - Project Nirvoya</title>
+                <meta name="description" content="Welcome to Project Nirvoya. Discover the best products, flash deals, and top categories." />
+        </Helmet>
         <Banner/>
         <Category/>
         <FlashDeals/>
