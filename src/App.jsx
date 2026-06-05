41d9681776467149f43ce7e726836f291d4cc70e
Sijan666 <666majharulislam@gmail.com>
2026-06-05 22:25:21 +0600

    remove unwanted codes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,3 @@
-
-// import Home from './components/pages/Home'
-// import { Routes , Route } from 'react-router-dom'
-// import RootLayouts from './components/layouts/RootLayouts'
-// import ProductDetails from './components/pages/ProductDetails'
-// import Allproducts from './components/pages/Allproducts'
-// // import About from './components/pages/About'
-// // import Error from './components/pages/Error'
-
-
-// function App () {
-//   return (
-//     <>
-//       <Routes>
-//         <Route path="/" element={<RootLayouts />}>
-//           <Route index element={<Home/>} />
-//           <Route path="/productDetails" element={<ProductDetails/>} />
-//           <Route path="/allproducts" element={<Allproducts/>} />
-//         </Route>
-//           {/* <Route path="*" element={<Error/>} /> */}
-//       </Routes>
-//     </>
-//   )
-// }
-
-// export default App
-
-
 import { Routes , Route } from 'react-router-dom'
 import React, { Suspense, lazy } from 'react'
 import RootLayouts from './components/layouts/RootLayouts'
